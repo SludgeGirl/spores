f79d218701a335e4947544fe5d174d1a7368f3f3
Sludge <alicembrooks@protonmail.com>
2026-02-23 20:57:30 +0000

    Use shellcheck's suggested changes for smudge

--- secrets_smudge.bash
+++ secrets_smudge.bash
@@ -3,7 +3,7 @@ if [ $# -gt 0 ]; then
   if [ ! -e secrets ]; then
     secrets="$(age -d -i ~/.ssh/id_ed25519 secrets.age)"
 
-    if [ "$?" = "0" ]; then
+    if $?; then
       echo "$secrets" > secrets
     else
       >&2 echo "Failed to decrypt secrets"
@@ -12,21 +12,21 @@ if [ $# -gt 0 ]; then
   fi
 
   output="$(</dev/stdin)"
-  while read secret_line; do
+  while read -r secret_line; do
     name=${secret_line%%=*}
     value=${secret_line#*=}
 
-	  if [ "$1" = "clean" ]; then
-      output="$(echo "$output" | sed -e "s/$value/$name/g")"
-	  elif [ "$1" = "smudge" ]; then
-      output="$(echo "$output" | sed -e "s/$name/$value/g")"
-	  fi
+    if [ "$1" = "clean" ]; then
+      output="${output//$value/$name}"
+    elif [ "$1" = "smudge" ]; then
+      output="${output//$name/$value}"
+    fi
   done <secrets
 
-	if [ "$output" != "" ]; then
-	  echo "$output"
-	  exit 0
-	fi
+  if [ "$output" != "" ]; then
+    echo "$output"
+    exit 0
+  fi
 fi
 
 >&2 echo "Unknown action, please use either filter or smudge"
